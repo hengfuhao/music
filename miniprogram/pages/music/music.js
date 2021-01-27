e2333d01ed34b7c039d594f1211ee96175901dc0
hengfuhao <1419744187@qq.com>
2021-01-27 14:10:36 +0800

    first commit

---
+++ miniprogram/pages/music/music.js
@@ -0,0 +1,127 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+imgUrls:[{
+    url:'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+  },
+  {
+    url:'http://p1.music.126.net/SLfispSeeEnb6Ezs0cNjBw==/109951165666128356.jpg?imageView&quality=89'
+  },
+  {
+  url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+  },
+  {
+    url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+      },
+     {
+      url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+     },
+     {
+       url:'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+     },
+     {
+    url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+     }
+],
+playlist:[{
+  "id":"1001",
+  "playCount":1.4641238e+06,
+  "name":"Pitchfork评选70年代最佳200首歌曲",
+  "picUrl":"http://p2.music.126.net/CObKLNue7cWdPMfbVsiOzA==/109951165666370374.jpg?param=140y140"
+},
+{
+"id": "1002",
+"playCount":1.4641238e+06,
+"name":"你一定要在自己热爱的世界里闪闪发亮啊",
+"picUrl":"http://p1.music.126.net/uesfHcJmZ23S3er_1mpeaw==/109951165621856219.jpg?param=140y140"
+},
+{
+"id":"1003",
+"playCount":1.4641238e+06,
+"name":"你数数 这是你第几次丢下我",
+"picUrl":"http://p1.music.126.net/Xvo6PwBcdOA69ipcpV9YYg==/109951165463253777.jpg?param=140y140"
+},
+{
+"id":"1004",
+"playCount":1.4641238e+06,
+"name":"【翻/原】温柔不是我说 而是你觉得.",
+"picUrl":"http://p1.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg?param=140y140"
+},
+{
+"id":"1005",
+"playCount":1788859.6,
+"name":"2021·心里装着鲜花银河星光和我爱的人",
+"picUrl":"http://p1.music.126.net/O8LkkfC7PtV7TA4UP693XA==/109951164569667332.jpg?param=140y140"
+},
+{
+"id":"1006",
+"playCount":1.4638e+06,
+"name":"谁不是一边委屈着，一边努力前行着",
+"picUrl":"http://p2.music.126.net/TCggwwBfE33obl7nZAQoRg==/109951165629612076.jpg?imageView=1&type=webp&thumbnail=247x0"
+}
+]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
